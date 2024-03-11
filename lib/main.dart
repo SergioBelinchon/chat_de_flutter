575c07a84aef1f779bf39b5b63a7dafdac24c2d2
sergio belinchon <sergiorayo12@gmail.com>
2024-03-11 13:03:28 +0100

    IMPLEMENTACION FIREBASE

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:chat_de_flutter/src/App.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() {
   runApp(const App());
